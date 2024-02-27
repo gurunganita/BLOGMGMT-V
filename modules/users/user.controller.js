db8351ddb94512e0442dd600f6833b81f26c28e9
Anita Gurung <gurunganita982@gmail.com>
2024-02-27 15:47:54 +0545

    pagination-concept

--- modules/users/user.controller.js
+++ modules/users/user.controller.js
@@ -1,7 +1,9 @@
 const userModel = require("./user.model");
-const { hashPassword, comparePassword } = require("../../utils/bcrypt");
 const mailer = require("../../services/mailer");
+const { hashPassword, comparePassword } = require("../../utils/bcrypt");
+const { signJWT, generateRandomToken } = require("../../utils/token");
 const register = async (payload) => {
+  delete payload.roles;
   payload.password = hashPassword(payload.password);
   const user = await userModel.create(payload);
   if (!user) throw new Error("Registration Failed");
@@ -35,26 +37,160 @@ const login = async (payload) => {
   // const token = await signJWT(signingData);
   // return token;
 };
-const getById = (_id) => {};
-const updateById = (_id, payload) => {};
-const forgetPassword = (payload) => {};
+const getAll = async (search, page = 1, limit = 2) => {
+  const query = [];
+  if (search?.name) {
+    query.push({
+      $match: {
+        name: new RegExp(search?.name, "gi"),
+      },
+    });
+  }
+  if (search?.email) {
+    query.push()({
+      $match: {
+        email: new RegExp(search?.email, "gi"),
+      },
+    });
+  }
+  query.push(
+    {
+      $facet: {
+        metadata: [
+          {
+            $count: "total",
+          },
+        ],
+        data: [
+          {
+            $skip: (+page - 1) * +limit,
+          },
+          {
+            $limit: +limit,
+          },
+        ],
+      },
+    },
+    {
+      $addFields: {
+        total: {
+          $arrayElemAt: ["$metadata.total", 0],
+        },
+      },
+    },
+    {
+      $project: {
+        metadata: 0,
+      },
+    }
+  );
+  const result = await userModel.aggregate(query);
+  return {
+    data: result[0].data,
+    total: result[0].total || 0,
+    page: +page,
+    limit: +limit,
+  };
+};
 
-const resetPassword = (userId, payload) => {};
-const changePassword = (userId, payload) => {};
-const getProfile = aync (_id)=>{
-  return userModel.findOne{_id};
-}
+const getById = (_id) => {
+  return userModel.findOne({ _id });
+};
+
+const updateById = (_id, payload) => {
+  return userModel.updateOne({ _id }, payload);
+};
+const generateFpToken = async (payload) => {
+  /*
+    1. in req.body (email)
+    2. check if user exist or not using email
+    3. send the email with recovery token 
+    4. store the token in the server as well
+    5. send the email to the user with token
+    5.compare the token
+    6. If token matches , ask for new password
+    7. hash the passworduser 
+    8. update the database passeord for that email
+    */
+  const { email } = payload;
+  if (!email) throw new Error("email is missing");
+  const user = await userModel.findOne({ email });
+  if (!user) throw new Error("user doesnot exists");
+  const randomToken = generateRandomToken(); //generating the token
+  await userModel.updateOne({ email }, { token: randomToken }); //store it to the database
+  const isEmailSent = await mailer(
+    user.email,
+    "Forget Password",
+    `Your token is ${randomToken}`
+  );
+  if (isEmailSent) return "forget password token sent successfully";
+};
+const verifyFpToken = async (payload) => {
+  const { token, email, password } = payload;
+  if (!token || !email || !password) throw new Error("Something is missing");
+  const user = await userModel.findOne({ email });
+  if (!user) throw new Error("user doesnot exists");
+  const { token: verifyToken } = user;
+  if (token !== verifyToken) throw new Error("Invalid Token");
+  const updateUser = await userModel.updateOne(
+    { email },
+    { password: hashPassword(password), token: "" }
+  );
+  return "Password updated Successfully";
+};
+
+const forgetPassword = (payload) => {};
+const resetPassword = (payload) => {
+  const { userId, password } = payload;
+  if (!userId || !password) throw new Error("UserId or Password id required");
+
+  return userModel.updateOne(
+    { _id: userId },
+    { password: hashPassword(password) }
+  );
+};
+
+const changePass = async (payload) => {
+  const { email, oldpassword, newpassword } = payload;
+  if ((!email || !oldpassword, !newpassword))
+    throw new Error("Something is missing");
+  const user = await userModel.findOne({ email }).select("+password");
+  if (!user) throw new Error("user doesnot exists");
+  const isValidOldPassword = comparePassword(oldpassword, user.password);
+  if (!isValidOldPassword) throw new Error("Password didnot match");
+  const newHashPw = hashPassword(newpassword);
+  await userModel.updateOne({ password: newHashPw });
+  return "Password Updated Successfully";
+};
+const create = async (payload) => {
+  return userModel.create(payload);
+};
+const getProfile = async (_id) => {
+  return userModel.findOne({ _id });
+};
 const updateProfile = async (_id, payload) => {
   delete payload.email;
   return userModel.findOne({ _id }, payload);
 };
-
+const blockUser = async (_id) => {
+  const user = await userModel.findOne({ _id });
+  if (!user) throw new Error("User not found");
+  const payload = { isactive: !user.isActive };
+  return userModel.updateOne({ _id }, payload);
+};
 module.exports = {
+  create,
+  getProfile,
+  updateProfile,
+  blockUser,
   register,
   login,
   getById,
+  getAll,
   updateById,
   forgetPassword,
   resetPassword,
-  changePassword,
+  changePass,
+  verifyFpToken,
+  generateFpToken,
 };
--- modules/users/user.route.js
+++ modules/users/user.route.js
@@ -1,9 +1,12 @@
 const router = require("express").Router();
+// const userController = require("./user.controller");
 const userController = require("./user.controller");
+const { checkRole } = require("../../utils/sessionManager");
+
 router.post("/register", async (req, res, next) => {
   try {
     const result = await userController.register(req.body);
-    res.json({ data: result });
+    res.json({ msg: result });
   } catch (e) {
     next(e);
   }
@@ -11,9 +14,113 @@ router.post("/register", async (req, res, next) => {
 router.post("/login", async (req, res, next) => {
   try {
     const result = await userController.login(req.body);
-    res.json({ data: result });
+    res.json({ msg: result });
   } catch (e) {
     next(e);
   }
 });
+router.post("/", checkRole(["admin"]), async (req, res, next) => {
+  try {
+    const result = await userController.create(req.body);
+    res.json({ msg: result });
+  } catch (e) {
+    next(e);
+  }
+});
+router.get("/", checkRole(["admin"]), async (req, res, next) => {
+  try {
+    const { name, phone, email, page, limit } = req.query;
+    const search = { name, phone, email };
+
+    const result = await userController.getAll(search, page, limit);
+    res.json({ msg: result });
+  } catch (e) {
+    next(e);
+  }
+});
+router.get(":/id", checkRole(["admin"]), async (req, res, next) => {
+  try {
+    const result = await userController.getById(req.params.id);
+    res.json({ msg: result });
+  } catch (e) {
+    next(e);
+  }
+});
+
+router.get("/get-profile", checkRole(["user"]), async (req, res, next) => {
+  try {
+    const result = await userController.getProfile(req.currentUser);
+    res.json({ msg: result });
+  } catch (e) {
+    next(e);
+  }
+});
+router.put("/update-profile", checkRole(["user"]), async (req, res, next) => {
+  try {
+    const result = await userController.updateProfile(
+      req.currentUser,
+      req.body
+    );
+    res.json({ msg: result });
+  } catch (e) {
+    next(e);
+  }
+});
+
+router.post("/generate-fp-token", async (req, res, next) => {
+  try {
+    const result = await userController.generateFpToken(req.body);
+    res.json({ msg: result });
+  } catch (e) {
+    next(e);
+  }
+});
+
+router.post("/verify-fp-token", async (req, res, next) => {
+  try {
+    const result = await userController.verifyFpToken(req.body);
+    res.json({ msg: result });
+  } catch (e) {
+    next(e);
+  }
+});
+router.post("/reset-password", checkRole(["admin"]), async (req, res, next) => {
+  try {
+    const result = await userController.resetPassword(req.body);
+    res.json({ msg: result });
+  } catch (e) {
+    next(e);
+  }
+});
+
+// router.post(
+//   "/change-password",
+//   checkRole[("admin", "user")],
+//   async (req, res, next) => {
+//     try {
+//       const result = await userController.changePass(req.body);
+//       res.json({ msg: result });
+//     } catch (e) {
+//       next(e);
+//     }
+//   }
+// );
+router.post("/", checkRole(["admin"]), async (req, res, next) => {
+  try {
+    const result = await userController.create(req.body);
+    res.json({ msg: result });
+  } catch (e) {
+    next(e);
+  }
+});
+
+router.put("/:id", checkRole(["admin"]), async (req, res, next) => {
+  try {
+    const result = await userController.updateById(req.params.id, req.body);
+    res.json({ msg: result });
+  } catch (e) {
+    next(e);
+  }
+});
+
 module.exports = router;
